37c1f59858315ef02495c3c6c1070c31c1e08ac6
Rutujeet Suryawanshi <rutujeets@gmail.com>
2024-08-13 22:48:21 +0530

    Added Post hooks

--- components/FeedCard/index.tsx
+++ components/FeedCard/index.tsx
@@ -3,33 +3,33 @@ import Image from 'next/image';
 import { GoComment, GoHeart, GoUpload } from "react-icons/go";
 import { LiaRetweetSolid } from "react-icons/lia";
 
-const FeedCard: React.FC =  () => {
+const FeedCard: React.FC = () => {
     return (
-    <div className="border-b-[1px] border-slate-700 p-4 cursor-pointer">
-<div className="grid grid-cols-12 gap-3">
-    <div className="col-span-1">
-        <Image src="https://i.redd.it/iysrp1i7afl31.png" alt="user-image" height={50} width={50} className="rounded-full"/>
-    </div>
-    <div className="col-span-11">
-        <h5>plugpollution</h5>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem aliquam odio eum sint porro nisi similique, laboriosam laborum itaque tenetur earum eligendi vitae quisquam omnis accusamus. Neque ratione nostrum corporis?</p>
-        <div className="flex justify-between text-xl items-center p-2 pr-10 w-[90%]">
-            <div>
-            <GoComment />
-            </div>
-            <div>
-            <LiaRetweetSolid className="text-2xl"/>
-            </div>
-            <div>
-            <GoHeart />
-            </div>
-            <div>
-            <GoUpload />
+        <div className="border-b-[1px] border-slate-700 p-4 cursor-pointer">
+            <div className="grid grid-cols-12 gap-3">
+                <div className="col-span-1">
+                    <Image src="https://i.redd.it/iysrp1i7afl31.png" alt="user-image" height={50} width={50} className="rounded-full" />
+                </div>
+                <div className="col-span-11">
+                    <h5>plugpollution</h5>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem aliquam odio eum sint porro nisi similique, laboriosam laborum itaque tenetur earum eligendi vitae quisquam omnis accusamus. Neque ratione nostrum corporis?</p>
+                    <div className="flex justify-between text-xl items-center p-2 pr-10 w-[90%]">
+                        <div>
+                            <GoComment />
+                        </div>
+                        <div>
+                            <LiaRetweetSolid className="text-2xl" />
+                        </div>
+                        <div>
+                            <GoHeart />
+                        </div>
+                        <div>
+                            <GoUpload />
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-</div>
-    </div>
     )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { BsTwitterX } from "react-icons/bs"
 import { GoHome, GoSearch, GoBell, GoMail, GoChecklist, GoBookmark, GoPeople } from "react-icons/go"
 import { FiUser } from "react-icons/fi"
 import { CgMoreO } from "react-icons/cg"
+import { GoFileMedia } from "react-icons/go";
 
 import FeedCard from "@/components/FeedCard"
 
@@ -36,18 +37,26 @@ const sidebarMenuItems: XSidebarButton[] = [
 
 export default function Home() {
 
-  const {user} = useCurrentUser()
+  const { user } = useCurrentUser()
+
+
+  const handleSelectMedia = useCallback(() => {
+    const input = document.createElement('input')
+    input.setAttribute("type", "file");
+    input.setAttribute("accept", "image/*")
+    input.click();
+  }, [])
 
   const handleLoginWithGoogle = useCallback(async (cred: CredentialResponse) => {
     const googleToken = cred.credential
     if (!googleToken) return toast.error(`Google token not found`)
-    const {verifyGoogleToken} = await graphqlClient.request(verifyUserGoogleTokenQuery, {token: googleToken})
+    const { verifyGoogleToken } = await graphqlClient.request(verifyUserGoogleTokenQuery, { token: googleToken })
 
 
     toast.success(`verified success`)
     console.log(verifyGoogleToken)
 
-    if(verifyGoogleToken) window.localStorage.setItem('__twitter_token', verifyGoogleToken)
+    if (verifyGoogleToken) window.localStorage.setItem('__twitter_token', verifyGoogleToken)
 
   }, [])
   return (
@@ -70,20 +79,38 @@ export default function Home() {
                 </li>
               ))}
             </ul>
-            
+
             {/* Post Button */}
             <div className="mt-5 pr-7 font-semibold">
               <button className="bg-sky-500 text-xl p-4 rounded-full w-full mt-5">Post</button>
             </div>
           </div>
-         {user && (<div className="absolute bottom-16 flex gap-2 bg-slate-800 px-3 py-2 rounded-full">
-          {user && user.profileImageUrl && (<Image className="rounded-full" src={user?.profileImageUrl} alt="profile-img" height={50} width={50} />)}
-          <h3 className="text-xl">{user.firstName} {user.lastName}</h3>
-         </div>)}
+          {user && (<div className="absolute bottom-16 flex gap-2 bg-slate-800 px-3 py-2 rounded-full">
+            {user && user.profileImageUrl && (<Image className="rounded-full" src={user?.profileImageUrl} alt="profile-img" height={50} width={50} />)}
+            <h3 className="text-xl">{user.firstName} {user.lastName}</h3>
+          </div>)}
         </div>
 
         {/* Main Content Area */}
         <div className="col-span-5 border-r-[1px] border-l-[1px] border-slate-700 overflow-scroll">
+          <div className="border-b-[1px] border-slate-700 p-4">
+            <div className="grid grid-cols-12 gap-3">
+              <div className="col-span-1">
+
+
+                {user?.profileImageUrl && (<Image src={user?.profileImageUrl} alt="user-image" height={50} width={50} className="rounded-full" />)}
+              </div>
+              <div className="col-span-11">
+                <textarea className="w-full bg-transparent text-2xl px-1 border-b border-slate-700" rows={4} name="" id="" placeholder="what's happening?"></textarea>
+                <div className="mt-1 flex justify-between items-center">
+                  <GoFileMedia onClick={handleSelectMedia} className="text-xl cursor-pointer" />
+                  <button className="bg-sky-500 text-sm px-4 py-2 rounded-full font-bold">Post</button>
+
+                </div>
+              </div>
+
+            </div>
+          </div>
           <FeedCard />
           <FeedCard />
           <FeedCard />
@@ -99,8 +126,8 @@ export default function Home() {
         <div className="col-span-3 p-5">
           {!user && (<div className="border p-5 bg-slate-700 rounded-lg">
             <h1 className="my-2 text-xl">New to X?</h1>
-           <GoogleLogin onSuccess={handleLoginWithGoogle}/> 
-           </div>) }
+            <GoogleLogin onSuccess={handleLoginWithGoogle} />
+          </div>)}
         </div>
       </div>
     </div>
